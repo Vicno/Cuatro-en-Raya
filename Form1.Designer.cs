d77eae0929d0fbc1e9e6fc6dcfac7fa27222afec
KOrtizLedezma <kenethortiz2016@gmail.com>
2019-10-14 14:35:10 -0400

    Musica de Fondo Insertada

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,27 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.BotonSonido = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // BotonSonido
+            // 
+            this.BotonSonido.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BotonSonido.Location = new System.Drawing.Point(535, 17);
+            this.BotonSonido.Name = "BotonSonido";
+            this.BotonSonido.Size = new System.Drawing.Size(57, 25);
+            this.BotonSonido.TabIndex = 0;
+            this.BotonSonido.Text = "Sound";
+            this.BotonSonido.UseVisualStyleBackColor = true;
+            this.BotonSonido.Click += new System.EventHandler(this.BotonSonido_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.Controls.Add(this.BotonSonido);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -43,6 +57,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button BotonSonido;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using EO.Internal;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,9 +8,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Media;
+
 
 namespace _4enRaya
 {
+
     public partial class Form1 : Form
     {
         public Form1()
@@ -19,6 +23,14 @@ namespace _4enRaya
 
         private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void BotonSonido_Click(object sender, EventArgs e)
+        {
+            SoundPlayer player = new SoundPlayer();
+            player.SoundLocation = @".\Queen - Bohemian Rhapsody WAV.wav";
+            player.PlayLooping();
 
         }
     }
